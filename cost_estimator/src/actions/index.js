410b1e052a48e11c9f66d65120fd807633911c65
Syed Ahmed Shah <ahmed.shah@illusionteck.com>
2021-10-15 14:14:46 +0400

    Add pricing actions created

--- cost_estimator/src/actions/index.js
+++ cost_estimator/src/actions/index.js
@@ -1,6 +1,23 @@
 import gateway from '../api';
 import { FETCH_PRICES } from './types';
 
+export const AddPrices = formValues => async (dispatch, getState) => {
+    try {
+        // SIMULATION OF ADD / CREATE PRICING API
+        const prices = formValues;
+
+        dispatch({
+            type: FETCH_PRICES,
+            payload: prices
+        });
+    } catch(err) {
+        dispatch({
+            type: FETCH_PRICES,
+            payload: { status: 500, type: 'error', msg: `We're sorry as we're unable to fetch the prices at the moment. Please try again later.` }
+        });
+    }
+};
+
 export const ListPrices = formValues => async (dispatch, getState) => {
     try {
         const response = await gateway.post('/prod', formValues);
