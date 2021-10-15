69e52e71172626d62728e22d5de065f94b7a1c79
Syed Ahmed Shah <ahmed.shah@illusionteck.com>
2021-10-15 14:00:18 +0400

    Header added & styled

--- cost_estimator/src/components/Header.js
+++ cost_estimator/src/components/Header.js
@@ -1,8 +1,17 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Fragment } from 'react';
 
 const Header = () => {
     return (
-        <div></div>
+        <Fragment>
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="">
+                        <img src="/assets/images/alasco-logo.png" alt="" width="192" height="30" className="d-inline-block align-text-top" />
+                    </a>
+                </div>
+            </nav>
+        </Fragment>
     );
 }
 
